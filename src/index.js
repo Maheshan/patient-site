b3ad945316f1d602624a46fc920928b6edc57ecc
Maheshan Vattekat <maheshan.vattekat@gmail.com>
2019-03-30 16:39:26 -0400

    Refactored to use async await for routes

--- src/index.js
+++ src/index.js
@@ -9,29 +9,26 @@ const port = process.env.PORT || 5000;
 // Parsing JSON
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+  try {
+    let users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get("/users/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    let user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 app.post("/login", (req, res) => {
