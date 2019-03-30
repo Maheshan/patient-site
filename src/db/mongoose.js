e756a6ed8c21f702b0195385d836c9e6db6739db
Maheshan Vattekat <maheshan.vattekat@gmail.com>
2019-03-30 15:38:08 -0400

    Refactored to use async await

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -11,64 +11,62 @@ mongoose.connect(connectionString, {
 Role.collection.drop();
 User.collection.drop();
 
-let seededRoles = [
-  {
-    role: "Admin"
-  },
-  {
-    role: "User"
-  }
-];
+const seedData = async () => {
+  let seededRoles = [
+    {
+      role: "Doctor"
+    },
+    {
+      role: "Patient"
+    }
+  ];
+  let roles = await Role.create(seededRoles);
+  let seededUsers = [
+    {
+      name: "Doogie Howser",
+      age: 14,
+      email: "dhowser@aol.com",
+      password: "tester1234567",
+      address: "Something",
+      phone: "1234567890",
+      role: roles[0]
+    },
+    {
+      name: "Rick Sanchez",
+      age: 70,
+      email: "wubbalubbadubdub@hotmail.com",
+      password: "IheartMeseeks23",
+      address: "Something",
+      phone: "1234567890",
+      role: roles[1]
+    },
+    {
+      name: "Dante",
+      age: 37,
+      email: "devilmayweep@gmail.com",
+      password: "betterthanvirgil25",
+      address: "Something",
+      phone: "1234567890",
+      role: roles[1]
+    },
+    {
+      name: "Motoko Kusanagi",
+      age: 30,
+      email: "hackerleet@gmail.com",
+      password: "Tachikoma123",
+      address: "Something",
+      phone: "1234567890",
+      role: roles[1]
+    }
+  ];
+  let users = await User.create(seededUsers);
+  return users;
+};
 
-Promise.all([Role.create(seededRoles)])
-  .catch(err => {
-    // log that I have an error
-    console.log("A promise failed to resolve", err);
+seedData()
+  .then(() => {
+    console.log("Users and roles seeded successfully.");
   })
-  .then(roles => {
-    let users = [
-      {
-        name: "Doogie Howser",
-        age: 14,
-        email: "dhowser@aol.com",
-        password: "tester1234567",
-        address: "Something",
-        phone: "1234567890",
-        role: roles[0][0]
-      },
-      {
-        name: "Rick Sanchez",
-        age: 70,
-        email: "wubbalubbadubdub@hotmail.com",
-        password: "IheartMeseeks23",
-        address: "Something",
-        phone: "1234567890",
-        role: roles[0][1]
-      },
-      {
-        name: "Dante",
-        age: 37,
-        email: "devilmayweep@gmail.com",
-        password: "betterthanvirgil25",
-        address: "Something",
-        phone: "1234567890",
-        role: roles[0][1]
-      },
-      {
-        name: "Motoko Kusanagi",
-        age: 30,
-        email: "hackerleet@gmail.com",
-        password: "Tachikoma123",
-        address: "Something",
-        phone: "1234567890",
-        role: roles[0][1]
-      }
-    ];
-    return User.create(users, (err, docs) => {
-      if (err) {
-        return console.log(err);
-      } else {
-        console.log("Multiple docs inserted into users");
-      }
-    });
+  .catch(err => {
+    console.log("Failed to seed data", err);
   });
