4eb4abeec244d914b015c364a2f71f03350d497a
Maheshan Vattekat <maheshan.vattekat@gmail.com>
2019-03-31 17:57:47 -0400

    Putting in rest

--- src/client/src/app.js
+++ src/client/src/app.js
@@ -1,6 +1,68 @@
-console.log("App.js is running!");
+class PatientsApp extends React.Component {
+  render() {
+    const title = "Patients Site";
+    const subtitle = "Subtitle";
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Login />
+      </div>
+    );
+  }
+}
 
-let template = <h1>Patient site</h1>;
-let appRoot = document.getElementById("app");
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(template, appRoot);
+class Action extends React.Component {
+  handleClick() {
+    alert("test");
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Tester</button>
+      </div>
+    );
+  }
+}
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  handleLogin(e) {
+    e.preventDefault();
+
+    let email = e.target.elements.useremail.value;
+    let password = e.target.elements.userpassword.value;
+    if (email && password) {
+      alert(email);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleLogin}>
+          <input type="email" name="useremail" />
+          <p />
+          <input type="password" name="userpassword" />
+          <p />
+          <button>Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<PatientsApp />, document.getElementById("app"));
